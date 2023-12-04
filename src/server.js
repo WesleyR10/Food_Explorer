372512be9131ba0475b9ce129c02f5f0defa33f5
WesleyR10 <wesleyribas2015@gmail.com>
2023-12-04 13:12:59 -0300

    att cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://wrfoodexplorer.netlify.app"],
   credentials: true
 }));
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
